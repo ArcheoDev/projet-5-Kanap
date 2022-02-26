57a6d90b428d6959298d8c9051ce8706a530935f
ArchéoDev <helene.labit@gmail.com>
2022-02-26 17:34:15 +0100

    Etape 6 ok

--- js/index.js
+++ js/index.js
@@ -1,15 +1,16 @@
 //Insérer les produits de l'API
 
 //Dans index.html, l. 117, le code source du script a été changé "/js/index.js"
-const URL = 'http://localhost:3000/api/products';       
-let section = document.getElementById('items'); 
-let html = "";
+const URL = 'http://localhost:3000/api/products';                               //constante pour l'URL de l'API car lien unique   
+let section = document.getElementById('items');                                 //permet de récupérer l'ID dans l'HTML
+let html = "";                                                                  //déclaration de la variable html
 
-fetch(URL)                                                                      //j'exécute des requêtes HTTP
+fetch(URL)                                                                      //je récupére les éléments et j'exécute des requêtes HTTP
     .then(res => res.json())                                                    //je récupére le résultat de la requête au format json
-    .then(function(kanaps) {                                                  
+    .then(function(kanaps) {       
+        console.log(kanaps);                                                    //récupération des éléments dans un tableau, 8 lignes car 8 canapés                                        
         kanaps.map((kanap => {                                                  //j'exécute une fonction sur chaque élément du tableau
-            html =                                                              //je reprends les éléments de l'HTML en commentaire et j'ajoute les différents types de données issues du pdf "spécifications fonctionnelles" 
+            html =                                                              //boucle, je reprends les éléments de l'HTML en commentaire et j'ajoute les différents types de données issues du pdf "spécifications fonctionnelles" 
             `<a href="./product.html?id=${kanap._id}">                          
                 <article>                                 
                     <img src="${kanap.imageUrl}" alt="${kanap.altTxt}">     
@@ -17,7 +18,7 @@ fetch(URL)                                                                      
                     <p class="productDescription">${kanap.description}</p>
                 </article>
             </a>`;
-        section.insertAdjacentHTML("beforeend", html);}))                       // insertAdjacentHTML : Alternative à innerHTML, plus rapide et directe / beforeend : position, chaine de caractère juste à l'intérieur de l'élément, après son premier enfant
+        section.insertAdjacentHTML("beforeend", html);}))                       // incère les noeuds dans le DOM/ insertAdjacentHTML : Alternative à innerHTML, plus rapide et directe / beforeend : position, chaine de caractère juste à l'intérieur de l'élément, après son premier enfant
     })                                                                          //insertion de l'HTML dans la section  /  une boucle, il va créer autant d'HTML qu'il y a de canapé
     .catch(err => console.log('Une erreur est survenue : ' + err))              //Si erreur, l'API ne fonctionne plus, console log alert
     ;
--- js/product.js
+++ js/product.js
@@ -1,10 +1,36 @@
 //Modifier l'URL de l'API
 
 const param = new URLSearchParams(document.location.search);        //je demande au DOM où on se trouve
-let id = param.get("id");                                           //récupérer le paramètre ID de l'url
-console.log(id);
+let id = param.get("id");                                           //je récupére le paramètre ID de l'url
+console.log(id);                                                    //récupération de l'id pour chaque canapé
 
-fetch(`http://localhost:3000/api/products/${id}`)
+fetch(`http://localhost:3000/api/products/${id}`)                   //récupération des éléments descriptif d'un canapé
     .then(res => res.json())
-    .then(kanap => 
-    console.log (kanap));
+    .then(k => {
+        console.log (k);
+        document.querySelector (".item__img").innerHTML=
+        `<img src="${k.imageUrl}" alt="${k.altTxt}">`;
+        document.getElementById ("title").innerHTML=
+        `<h1 id="title">${k.name}</h1>`
+        document.getElementById ("price").innerHTML=
+        `<span id="price">${k.price}</span>`
+        document.getElementById ("description").innerHTML=
+        `<p id="description">${k.description}</p>`
+        let colors=k.colors;
+        colors.forEach((c => {
+            c = document.getElementById ("colors").innerHTML+=
+            `<option value="${c}">${c}</option>`
+        }))
+      }
+    );
+    
+    document.getElementById ("colors").addEventListener(
+        "change", (event) =>
+        colors = event.target.value)
+    
+    document.getElementById ("quantity").addEventListener(
+        "change", (event) => {
+        quantity = parseInt(event.target.value);
+        console.log(quantity);
+        }
+        )
