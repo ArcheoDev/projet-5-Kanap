96fd9b5dd642c34ed515df3989dd68e7d103edc2
ArchéoDev <helene.labit@gmail.com>
2022-02-27 22:09:19 +0100

    Etape7 ok

--- js/product.js
+++ js/product.js
@@ -4,6 +4,9 @@ const param = new URLSearchParams(document.location.search);        //je demande
 let id = param.get("id");                                           //je récupére le paramètre ID de l'url
 console.log(id);                                                    //récupération de l'id pour chaque canapé
 
+let Canap = new Object();
+
+
 fetch(`http://localhost:3000/api/products/${id}`)                   //récupération des éléments descriptif d'un canapé
     .then(res => res.json())
     .then(k => {
@@ -17,7 +20,7 @@ fetch(`http://localhost:3000/api/products/${id}`)                   //récupéra
         document.getElementById ("description").innerHTML=
         `<p id="description">${k.description}</p>`
         let colors=k.colors;
-        colors.forEach((c => {
+        colors.map((c => {
             c = document.getElementById ("colors").innerHTML+=
             `<option value="${c}">${c}</option>`
         }))
@@ -31,6 +34,56 @@ fetch(`http://localhost:3000/api/products/${id}`)                   //récupéra
     document.getElementById ("quantity").addEventListener(
         "change", (event) => {
         quantity = parseInt(event.target.value);
-        console.log(quantity);
+        })
+
+    document.getElementById ("addToCart").addEventListener(
+        "click", (event) => {
+        event.preventDefault();
+        if (quantity > 0 && quantity <=100 && colors != undefined && colors != "none" ) {
+            const param = new URLSearchParams(document.location.search); 
+            let id = param.get("id");  
+            Canap.id = id;
+            Canap.quantity = quantity;
+            Canap.colors = colors;
+            addPanier(Canap);
+            } else {
+                alert(" il n'y a rien dans le panier");
+            }
+
         }
-        )
+    )
+
+    function getPanier () {
+        let panier = localStorage.getItem("panier");
+         return  panier < 1 ? [] : JSON.parse(panier);
+            // if (panier < 1) {
+            //    return [];         
+            // }
+            // else {
+            //     return JSON.parse(panier)
+            // }
+    }
+
+      function setPanier (p) {
+        localStorage.setItem('panier', JSON.stringify(p));
+    } 
+
+    function addPanier (produit) {
+        let panier = getPanier();
+        let check = false;
+        panier.map(item => {
+            if(item.id == produit.id && item.colors == produit.colors) {
+                // Si il y a un kanap (id + couleur) identique alors on incrémente le localstorage
+                check = true;
+                item.quantity += parseInt(produit.quantity);
+            }  
+        });
+        // Si il n'y a pas de kanap (id + couleur) identique alors on ajoute un clé 
+        if (check == false) {
+            panier.push(produit);  
+        }
+        setPanier(panier);
+    }
+
+  
+    
