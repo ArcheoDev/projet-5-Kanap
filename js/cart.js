341efaac16b22de296083b815981fb3ef7a40cfb
ArchéoDev <helene.labit@gmail.com>
2022-03-04 14:48:26 +0100

    card

---
+++ js/cart.js
@@ -0,0 +1,137 @@
+
+let panier = JSON.parse(localStorage.getItem('panier'));  
+
+let html = "";
+let section = document.getElementById('cart__items');
+
+// Création des variables du produit kanap
+let kanap = new Object();
+// Variable liée au event 
+let quantity;
+let colors;
+let kanapQuantite = document.getElementsByClassName("itemQuantity");
+
+// variable pour la fonction prix
+let compteurPrix = 0;
+let prixTotal = 0;  
+
+var compteur = 0;
+
+
+const promises = panier.map(i => {
+    return fetch(`http://localhost:3000/api/products/` + i.id)
+    .then(resp => {
+      return resp.json();
+       
+    });  
+});
+
+Promise.all(promises)
+  .then(kanaps => {
+    kanaps.forEach(kanap => {      
+    displayCanap(kanap);  
+    updateQuantity();
+    deleteCanap();
+    totalQuantite();
+    totalPrix(kanap);
+    compteur++;   
+  });
+});
+
+
+// Affichage produits
+function displayCanap(kanap) {
+  html = html + `
+    <article class="cart__item" data-id="${kanap._id}" data-color="${panier[compteur].colors}">
+      <div class="cart__item__img">
+          <img src="${kanap.imageUrl}" alt="${kanap.altTxt}">
+      </div>
+      <div class="cart__item__content">
+            <div class="cart__item__content__description">
+              <h2>${kanap.name}</h2>
+              <p>${panier[compteur].colors}</p>
+              <p>${kanap.price} €</p>
+            </div>
+            <div class="cart__item__content__settings">
+              <div class="cart__item__content__settings__quantity">
+                <p>Qté : </p>
+                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${panier[compteur].quantity}">
+              </div>
+              <div class="cart__item__content__settings__delete">
+                <p class="deleteItem">Supprimer</p>
+              </div>
+            </div>
+          </div>
+    </article>`; 
+  section.innerHTML = html;
+}
+
+// Panier vide retour Accueil
+function backPageHome() {
+  if (panier == null) {
+    alert('Le panier est vide ! retour accueil !');
+    location.href = 'index.html';
+  } 
+}
+
+// Changement quantité KANAP par utilisateur en direct sur le panier
+function updateQuantity() {
+  const kanapQuantity = document.querySelectorAll('.itemQuantity');
+  for (let i = 0; i < kanapQuantity.length; i++) {
+    kanapQuantity[i].addEventListener('change', (event) => {
+      event.preventDefault();
+      const kanapNewQuantity = event.target.value;
+      const newPanier = {
+        id: panier[i].id,
+        colors: panier[i].colors,
+        quantity: kanapNewQuantity,
+        price: panier[i].price,
+      };
+      panier[i] = newPanier;
+      localStorage.clear();
+      localStorage.setItem('panier',JSON.stringify(panier));
+      location.reload();
+    });
+  }
+}
+
+//Supprimer un produit kanap
+function deleteCanap() {  
+  const supprKanap = document.querySelectorAll('.deleteItem');  
+  for (let i = 0; i < supprKanap.length; i++) {   
+    
+    supprKanap[i].addEventListener('click', (event) => {
+      event.preventDefault();
+      panier.splice(i, 1);     
+      localStorage.setItem('panier',JSON.stringify(panier));
+      alert("Suppression de votre Kanap !")
+      location.reload();
+      if (localStorage.getItem("panier.value") == null) {
+        alert(`Remplissez votre panier ! retour à l'accueil ;)`);
+        document.location.href = `index.html`;    
+        localStorage.clear();
+      }
+    })
+  } 
+};
+
+//Total des articles 
+function totalQuantite() {    
+  let kanapTotal = 0;  
+
+  for(let k = 0; k < kanapQuantite.length; k++) {
+    kanapTotal += kanapQuantite[k].valueAsNumber;
+  }  
+  let kanapTotalQuantite = document.getElementById("totalQuantity");
+  kanapTotalQuantite.innerHTML = kanapTotal; 
+}
+
+//Total des prix
+function totalPrix(kanap) {
+  
+  prixTotal += kanapQuantite[compteurPrix].valueAsNumber * kanap.price;
+
+  let prixTotalKanap = document.getElementById("totalPrice");
+  prixTotalKanap.innerHTML = prixTotal;
+  compteurPrix++;     
+}
