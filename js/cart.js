3750d6460b7c276934f2af2cc1ff04dafda7264a
ArchéoDev <helene.labit@gmail.com>
2022-03-24 07:45:04 +0100

    retrait des commentaires perso

--- js/cart.js
+++ js/cart.js
@@ -1,29 +1,25 @@
-//Récupérer les données du localStorage
+let panier = JSON.parse(localStorage.getItem('panier'));
+let html = "";
+let section = document.getElementById('cart__items');
 
-//On va récuéprer les valeurs qu'il y a dans le localstorage
-
-let panier = JSON.parse(localStorage.getItem('panier'));    //il va récupérer les valeurs qu'il y a dans le localstorage 
-let html = "";                                              //pour afficher les produits dans le panier, string vide
-let section = document.getElementById('cart__items');       //variable, va récupérer dans le html où se trouve la carte "cart items"
-
-let kanap = new Object();                                   //création d'une variable de type objet, pour stocker des données à l'intérieur
-let canapQuantite = document.getElementsByClassName("itemQuantity");  //variable qui va récupérer dans le html la class itemQuantity
-let compteurPrix = 0;         // 0 car c'est le minimum
+let kanap = new Object();
+let canapQuantite = document.getElementsByClassName("itemQuantity");
+let compteurPrix = 0;
 let prixTotal = 0;  
 let compteur = 0;
 
-const promises = panier.map(i => {                                  //on a mapper sur le panier / récupération des valeurs dans le localstorage
-    return fetch(`http://localhost:3000/api/products/` + i.id)      //pour récupérer les données de chaque produit
-    .then(resp => {                                                 //réponse : donne les données d'un produit
+const promises = panier.map(i => {
+    return fetch(`http://localhost:3000/api/products/` + i.id)
+    .then(resp => {
       return resp.json();
-    });  
+    });
 });
 
-Promise.all(promises)             //chronologie de l'appel
-  .then(kanaps => {               //a récupéré les données de la promesse
-    kanaps.forEach(kanap => {     //données des produits mis dans le panier, itinéré par forEach 
-    displayCanap(kanap);          //les données d'un produit ont été utilisées comme un paramètre à l'interieur d'une fonction displayCanap
-    updateQuantity();             //si mise à jour de la quantité, la promesse va être déclenchée, boucle
+Promise.all(promises)
+  .then(kanaps => {
+    kanaps.forEach(kanap => {
+    displayCanap(kanap);
+    updateQuantity();
     deleteCanap();
     totalQuantite();
     totalPrix(kanap);
@@ -48,7 +44,6 @@ function displayCanap(kanap) {
   let productImg = document.createElement("img");
   productDivImg.appendChild(productImg);
   productImg.src = kanap.imageUrl;
-  // productImg.alt = productLocalStorage.altImgProduit;
   
   // Insertion de l'élément "div" pour la description produit
   let productItemContent = document.createElement("div");
@@ -86,7 +81,7 @@ function displayCanap(kanap) {
   productItemContentSettings.appendChild(productItemContentSettingsQuantity);
   productItemContentSettingsQuantity.className = "cart__item__content__settings__quantity";
   
-  // Insertion de "Qté : "
+  // Insertion de "Qté" : "
   let productQty = document.createElement("p");
   productItemContentSettingsQuantity.appendChild(productQty);
   productQty.innerHTML = "Qté : ";
@@ -115,33 +110,6 @@ function displayCanap(kanap) {
   section.appendChild(productArticle);
 }
 
-// Afficher les données de chaque produit dans le panier
-/*function displayCanap(kanap) {
-  html = html + `
-    <article class="cart__item" data-id="${kanap._id}" data-color="${panier[compteur].colors}">
-      <div class="cart__item__img">
-          <img src="${kanap.imageUrl}" alt="${kanap.altTxt}">
-      </div>
-      <div class="cart__item__content">
-            <div class="cart__item__content__description">
-              <h2>${kanap.name}</h2>
-              <p>${panier[compteur].colors}</p>
-              <p>${kanap.price} €</p>
-            </div>
-            <div class="cart__item__content__settings">
-              <div class="cart__item__content__settings__quantity">
-                <p>Qté : </p>
-                <input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${panier[compteur].quantity}">
-              </div>
-              <div class="cart__item__content__settings__delete">
-                <p class="deleteItem">Supprimer</p>
-              </div>
-            </div>
-          </div>
-    </article>`; 
-  section.innerHTML = html;
-}*/
-
 // Afficher une alerte pour retourner à la page d'accueil
 function backPageHome() {
     alert('Le panier est vide ! retour à la case départ : accueil ');
@@ -150,83 +118,80 @@ function backPageHome() {
 
 
 //Supprimer un produit
-function deleteCanap() {                                
-  const supprKanap = document.querySelectorAll('.deleteItem');    //va récupérer la localisation de tous les delateItem  
-  for (let i = 0; i < supprKanap.length; i++) {                   //boucle for sur le nombre total de la selection
+function deleteCanap() {
+  const supprKanap = document.querySelectorAll('.deleteItem');
+  for (let i = 0; i < supprKanap.length; i++) {
     
-    supprKanap[i].addEventListener('click', (event) => {          //avec l'évenement click on peut récupérer la selection du canapé [i] qu'on souhaite supprimer
-      event.preventDefault();                                     //pour ne pas avoir d'effets indésirables
-      panier.splice(i, 1);                                        //on supprime le produit [i] une fois
-      localStorage.setItem('panier',JSON.stringify(panier));      //on modifie et on enregistre les item du panier
-      !panier.length ? backPageHome() : displayAlert();           //deux conditions : Si nombre supérieur à 1, il va afficher le displayAlert (mise à jour de la page), si 0, affichage du backPageHome
+    supprKanap[i].addEventListener('click', (event) => {
+      event.preventDefault();
+      panier.splice(i, 1);
+      localStorage.setItem('panier',JSON.stringify(panier));
+      !panier.length ? backPageHome() : displayAlert();
     })
   } 
 }
 
-function displayAlert() {                                         // mise à jour de la page
+function displayAlert() {
   alert("Suppression de votre Kanap !");
   location.reload();
 }
 
 // Changement de la quantité par le client, page panier
 function updateQuantity() {
-  const kanapQuantity = document.querySelectorAll('.itemQuantity');   //va récupérer toutes les selections de quantité .itemQuantity
-  for (let i = 0; i < kanapQuantity.length; i++) {                    //boucle for pour itiner le nombre qui représente kanapQuantity.length
-    kanapQuantity[i].addEventListener('change', (event) => {          //on récupére l'index du bouton qui va changer la quantité, en utilisant evenement change
-      event.preventDefault();                                         //pour ne pas avoir d'effet indésirable
-      const kanapNewQuantity = event.target.value;                    //pour récupérer la valeur de chaque événement
-      const newPanier = {                                             //quantity : kanapNewQuantity pour remplacer la nouvelle quantité
+  const kanapQuantity = document.querySelectorAll('.itemQuantity');
+  for (let i = 0; i < kanapQuantity.length; i++) {
+    kanapQuantity[i].addEventListener('change', (event) => {
+      event.preventDefault();
+      const kanapNewQuantity = event.target.value;
+      const newPanier = {
         id: panier[i].id,
         colors: panier[i].colors,
         quantity: kanapNewQuantity,
         price: panier[i].price,
       };
-      panier[i] = newPanier;                                          //résultat : panier mis à jour [i] panier qu'on a récupéré après l'évenement
-      localStorage.setItem('panier',JSON.stringify(panier));          // setItem, pour modifier le localStorage
-      location.reload();                                              // afin de mettre à jour l'affichage de la quantité    
+      panier[i] = newPanier;
+      localStorage.setItem('panier',JSON.stringify(panier));
+      location.reload(); 
     });
   }
 }
 
-//quantité total 
-function totalQuantite() {              //calcule la quantité totale des articles
-  let kanapTotal = 0;                   //instancié à 0 / pas de produit = 0 
+function totalQuantite() {              
+  let kanapTotal = 0;                   
 
-  for(let k = 0; k < canapQuantite.length; k++) {     //boucle for / pour récuperer le nombre de kanap
-    kanapTotal += canapQuantite[k].valueAsNumber;     // on incrémente chaque produit ajouté
+  for(let k = 0; k < canapQuantite.length; k++) {     
+    kanapTotal += canapQuantite[k].valueAsNumber;    
   }  
-  let kanapTotalQuantite = document.getElementById("totalQuantity");  //kanapTotalQuantite de l'HTML, 
-  kanapTotalQuantite.innerHTML = kanapTotal;                          //ajout de "totalQuantité" 
+  let kanapTotalQuantite = document.getElementById("totalQuantity");  
+  kanapTotalQuantite.innerHTML = kanapTotal;                         
 }
 
 //prix total
-function totalPrix(kanap) {           //calcule le prix total des articles à partir du paramètre Kanap
+function totalPrix(kanap) {
   
-  prixTotal += canapQuantite[compteurPrix].valueAsNumber * kanap.price;  //le prix total va être récupérer et mis à jour / quantité x prix
+  prixTotal += canapQuantite[compteurPrix].valueAsNumber * kanap.price;
 
-  let prixTotalKanap = document.getElementById("totalPrice");     //on a chercher dans le html l'id totalPrice
-  prixTotalKanap.innerHTML = prixTotal;                           // ajout du prix calculé
-  compteurPrix++;                                                 //promesse : il va ajouter chaque produit selectionné
+  let prixTotalKanap = document.getElementById("totalPrice");
+  prixTotalKanap.innerHTML = prixTotal;
+  compteurPrix++;
 }
 
 
 //Formulaire
-const formulaire = document.querySelector('.cart__order__form');  //on a récupérer la selection .cart.... pour le formulaire
-let regex = /^[-'a-zA-ZÀ-ÖØ-öø-ÿ\s]{2,}$/;                        //test pour vérifier s'il y a au moins 2 caractéres dans un mot
-let regexLocal= /^[-'a-zA-Z0-9À-ÖØ-öø-ÿ\s]{2,}$/;                 //idem
-let regexEmail = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;   //test type mail
-let prenomValide = false;                                         //on les a instencié avec false, si le test est valide, ça passe à true
+const formulaire = document.querySelector('.cart__order__form');
+let regex = /^[-'a-zA-ZÀ-ÖØ-öø-ÿ\s]{2,}$/;
+let regexLocal= /^[-'a-zA-Z0-9À-ÖØ-öø-ÿ\s]{2,}$/;
+let regexEmail = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
+let prenomValide = false;
 let nomValide = false;
 let adresseValide = false;
 let villeValide = false;
 let emailValide = false;
 
-//ça marche, mais c'est mieux d'avoir une fonction pour chaque fonctionnalité indépendante
-
 //Contrôler le formulaire
-formulaire.firstName.addEventListener('input', function(){            //evenement type input avec id firstName
-    this.nextElementSibling.innerHTML = regex.test(this.value) ? "" : "Saisissez au moins deux caractères";  //fonction JS pour tester regex.test / this.value : valeur input / 2 conditions : si test accepté "", si pas accepté message d'erreur
-    prenomValide = regex.test(this.value);             //deux conditions : il va nous retourner true ou false
+formulaire.firstName.addEventListener('input', function(){
+    this.nextElementSibling.innerHTML = regex.test(this.value) ? "" : "Saisissez au moins deux caractères";
+    prenomValide = regex.test(this.value);
 });
 
 formulaire.lastName.addEventListener('input', function() {
@@ -250,7 +215,7 @@ formulaire.email.addEventListener('input', function() {
 });
 
 // Envoie du formulaire
-function verifForm() {          //si tout est valide = true / sinon affiche une alert
+function verifForm() {
     if (
         prenomValide &&
         nomValide &&
@@ -266,7 +231,7 @@ function verifForm() {          //si tout est valide = true / sinon affiche une 
 }
 
 //Données d'une commande
-let orderDetail = {                 //pour récupérer toutes les données du formulaire
+let orderDetail = {
     contact: {
         firstName: firstName,
         lastName: lastName,
@@ -278,26 +243,27 @@ let orderDetail = {                 //pour récupérer toutes les données du fo
 };
 
 //Enregistrer une commande
-const commander = document.getElementById('order');     //récupérer de l'id order
 
-commander.addEventListener("click", (event) => {        //avec un évenement click sur le bouton id "order"
-                event.preventDefault();                 //retirer l'événement
-                if (verifForm()) {                      //si tous les champs sont valides : true
-                        let post = {                  //une api peut avoir 4 méthodes, dont post
-                            method: 'POST',             //post = 
-                            headers: {                  //entête et contenu du message (body)
-                                'content-Type': 'application/json'}, //on envoie des données de type Json
-                            body: JSON.stringify(orderDetail),  //ce qu'on a récupéré est transformé en Json
+const commander = document.getElementById('order');
+
+commander.addEventListener("click", (event) => {
+                event.preventDefault();
+                if (verifForm()) {
+                        let post = {
+                            method: 'POST',
+                            headers: {
+                                'content-Type': 'application/json'},
+                            body: JSON.stringify(orderDetail),
                             };
-                        fetch("http://localhost:3000/api/products/order", post)   //adresse ou on doit envoyer le post
-                            .then((response) => response.json())                   //réponse de la demande
+                        fetch("http://localhost:3000/api/products/order", post)
+                            .then((response) => response.json())
                             .then(data => {
                                 // console.log(data);
-                                localStorage.setItem("orderId", data.orderId);    //après l'enregistrement de la commande, création d'un id de commande
-                                document.location.href = `confirmation.html`;     //renvoie à la page confirmation.html
+                                localStorage.setItem("orderId", data.orderId);
+                                document.location.href = `confirmation.html`;
                             });
                 } else {
-                    alert("Veuillez corriger le formulaire");  //si le verifForm n'est pas valide, demande de correction
+                    alert("Veuillez corriger le formulaire");
                 }
 });
 
--- js/index.js
+++ js/index.js
@@ -1,22 +1,6 @@
-//Insérer les produits de l'API
-
-
-// On a un lien API qu'on a mis dans une constante URL. Ce lien nous donne les données du produit
-// Le document.getElementById va récuperer les éléments avec un ID. On l'a mis dans une variable qui s'appelle let section
-// Déclare la variable de html, de type string ("")
-
 const URL = 'http://localhost:3000/api/products';
 let section = document.getElementById('items');
 let html = "";
-
-// On utilise Fetch, qui fait appel à l'URL pour récupérer les ressources et pour manipuler les requêtes et les réponses 
-// Les résultats de la requête sont récupérés en format json
-// On récupére les éléments dans un tableau, puis on exécute une fonction sur chaque élément du tableau, au moyen d'une boucle (map)
-// La variable HTML de type string, on a fait des concatations et les backtic ${}, ce qui permet de travailler sur chaque variable de produit 
-// Les éléments de l'HTML en commentaire ont été repris, et on a ajouté les différents types de données issues du pdf "spécifications fonctionnelles"  
-// L'inserAdjacentHTML a été utilisé pour incérer les données de l'HTML dans le DOM. Ceci est un alternative à innerHTML, car il est plus rapide et direct
-// Grace à la boucle, il y aura la création d'autant d'HTML qu'il y a de canapé
-// Si erreur, l'API ne fonctionne plus et un message d'alerte apparait
 
 const getProductList = async () => {
     const result = await fetch(URL);
@@ -27,9 +11,7 @@ const getProductList = async () => {
         const itemTitleElement = document.createElement("h3");
         const itemTextElement = document.createElement("p");
         const itemArticle = document.createElement("article");
-    
-    //    itemElement.setAttribute("class", "item-product");
-    
+        
         itemImageElement.setAttribute("src", productItem.imageUrl);
         itemTitleElement.textContent = productItem.name;
         itemTextElement.textContent = productItem.description;
@@ -46,20 +28,3 @@ const getProductList = async () => {
   };
 
   getProductList();
-
-/*fetch(URL)
-    .then(res => res.json())
-    .then(function(kanaps) {
-        // console.log(kanaps);
-        kanaps.map((kanap => {
-            html =
-            `<a href="./product.html?id=${kanap._id}">                          
-                <article>                                 
-                    <img src="${kanap.imageUrl}" alt="${kanap.altTxt}">     
-                    <h3 class="productName">${kanap.name}</h3>
-                    <p class="productDescription">${kanap.description}</p>
-                </article>
-            </a>`;
-        section.insertAdjacentHTML("beforeend", html);}))
-    })
-    .catch(err => console.log('Une erreur est survenue : ' + err)) */
